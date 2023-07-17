cb14f9f0578858e07b005238fbf5980ed51e80bb
Matthew Berthoud <mwberthoud@wm.edu>
2023-07-17 14:27:04 -0500

    Basic nav stuff works

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_session import Session
 # from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from decorators import login_required
+from helpers import login_required, usd
 
 # Configure application
 app = Flask(__name__)
@@ -19,14 +19,37 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///poker.db")
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        return redirect("/play")
+    else:
+        return render_template("index.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    session.clear()
+    if request.method == "POST":
+        username = request.form.get("username")
+        rows = db.execute("SELECT * FROM players WHERE username = ?", username)
+        p1 = request.form.get("password")
+        p2 = request.form.get("confirmation")
+        if not username:
+            return "must provide username"
+        elif len(rows) > 0:
+            return "username already exists"
+        elif not p1:
+            return "must provide password"
+        elif p1 != p2:
+            return "passwords do not match"
+        db.execute("INSERT INTO players (username, pswd_hash) VALUES(?, ?)", username, generate_password_hash(p1))
+        session["user_id"] = db.execute("SELECT * FROM players WHERE username = ?", username)[0]["player_id"]
+        return redirect("/")
+    else:
+        return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -38,25 +61,25 @@ def login():
         elif not request.form.get("password"):
             return "must provide password"
         rows = db.execute("SELECT * FROM players WHERE username = ?", request.form.get("username"))
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(rows[0]["pswd_hash"], request.form.get("password")):
             return "invalid username and/or password"
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0]["player_id"]
         return redirect("/")
     else:
         return render_template("login.html")
 
 
 @app.route("/logout")
+@login_required
 def logout():
     session.clear()
     return redirect("/")
 
-
 @app.route("/play")
+@login_required
 def play():
     return render_template("play.html")
-
 
 
 if __name__ == "__main__":
--- helpers.py
+++ helpers.py
@@ -1,3 +1,4 @@
+from flask import redirect, render_template, session
 from functools import wraps  # https://docs.python.org/3/library/functools.html#functools.wraps
 
 def login_required(f):  # https://flask.palletsprojects.com/en/2.3.x/patterns/viewdecorators/
@@ -12,3 +13,6 @@ def login_required(f):  # https://flask.palletsprojects.com/en/2.3.x/patterns/vi
 def admin_only(f):  # make this a thing if I ever add any ways to manage stuff from the backend
                     # for example if I want to add money to people's accounts or whatever
     pass
+
+def usd(value):
+    return f"${value:,.2f}"
